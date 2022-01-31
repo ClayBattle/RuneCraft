4b6eeed88d0b3ef988f8e85b51dfe6105b81ca02
Clay <claybattle12@gmail.com>
2022-01-31 17:52:09 -0500

    MDKinit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RuneCraft
+Minecraft mod aimed at adding skills, custom combat, elemental runes, and other stuff to the main game. Attempt 2.
---
+++ src/main/java/com/goldenugh/runecraft/RuneCraft.java
@@ -0,0 +1,13 @@
+package com.goldenugh.runecraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RuneCraft.MODID)
+public class RuneCraft {
+	public static final String MODID = "runecraft";
+
+	
+	public RuneCraft() {}
+	
+	
+}
